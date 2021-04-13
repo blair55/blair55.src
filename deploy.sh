b5903ef0a80786700b9d50745b1fed15b140bd81
Blair55 <blair.nicholas@googlemail.com>
2021-04-13 21:50:18 +0100

    Fix deploy.sh warnings

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-cd public
+cd public || true
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="rebuilding site $(date)"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
